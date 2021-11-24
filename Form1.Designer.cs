a5ab08f918efc6a87c42f27ca9eda59f1a447819
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-11-24 20:05:40 +0700

    Стартуем

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,176 @@ namespace Laba5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.calculate = new System.Windows.Forms.Button();
+            this.function = new System.Windows.Forms.TextBox();
+            this.upper = new System.Windows.Forms.TextBox();
+            this.lower = new System.Windows.Forms.TextBox();
+            this.integral = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(409, 138);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(379, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(12, 355);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(167, 83);
+            this.calculate.TabIndex = 1;
+            this.calculate.Text = "Рассчитать";
+            this.calculate.UseVisualStyleBackColor = true;
+            // 
+            // function
+            // 
+            this.function.Location = new System.Drawing.Point(51, 154);
+            this.function.Name = "function";
+            this.function.Size = new System.Drawing.Size(161, 20);
+            this.function.TabIndex = 2;
+            // 
+            // upper
+            // 
+            this.upper.Location = new System.Drawing.Point(18, 101);
+            this.upper.Name = "upper";
+            this.upper.Size = new System.Drawing.Size(17, 20);
+            this.upper.TabIndex = 3;
+            // 
+            // lower
+            // 
+            this.lower.Location = new System.Drawing.Point(4, 191);
+            this.lower.Name = "lower";
+            this.lower.Size = new System.Drawing.Size(17, 20);
+            this.lower.TabIndex = 4;
+            // 
+            // integral
+            // 
+            this.integral.AutoSize = true;
+            this.integral.BackColor = System.Drawing.Color.Transparent;
+            this.integral.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.integral.ForeColor = System.Drawing.Color.Black;
+            this.integral.Location = new System.Drawing.Point(8, 124);
+            this.integral.Name = "integral";
+            this.integral.Size = new System.Drawing.Size(37, 55);
+            this.integral.TabIndex = 5;
+            this.integral.Text = "∫";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(151, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Метод прямоугольников";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 50);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(108, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Метод трапеции";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 73);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(112, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Метод Симпсона";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.integral);
+            this.Controls.Add(this.lower);
+            this.Controls.Add(this.upper);
+            this.Controls.Add(this.function);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox function;
+        private System.Windows.Forms.TextBox upper;
+        private System.Windows.Forms.TextBox lower;
+        private System.Windows.Forms.Label integral;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
 
